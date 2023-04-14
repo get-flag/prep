d93ad770638714b8da41e0b14fbaeda8b1d59b78
sameer18ks <sameer.pythonistic@gmail.com>
2023-04-14 00:00:45 -0700

    Adding rev for annotaions and some notes for

---
+++ python/functional/doc_strings.py
@@ -0,0 +1,9 @@
+def  add(a: int,b :int) -> int:
+    '''Function to add two numbers
+    adding functional annotations and return type as well'''
+    
+    return add(a,b)
+
+
+#print(help(add))
+print(add.__doc__)
